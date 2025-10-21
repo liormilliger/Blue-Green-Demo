35b47f1f31c64081f3bb80596786a3a030f3ea4a
Lior Milliger <liormilliger@gmail.com>
2025-10-21 17:47:36 +0300

    Setting user least privileged permissions and creds

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
-provider "aws" {
-  region = var.aws_region
-  # data "aws_secretsmanager_secret_version" "aws_creds" {
-  #   secret_id = "your/secret/id"
-  # }
+variable "aws_region" {
+  description = "The AWS region for the deployment."
+  type        = string
+  default     = "us-east-1"
 }
+
+variable "blue-green-user-secret" {
+  description = "The ARN of the AWS Secrets Manager secret holding the IAM credentials."
+  type        = string
+}
+
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = var.blue-green-user-secret
+}
+
+locals {
+  aws_credentials = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = local.aws_credentials.access_key
+  secret_key = local.aws_credentials.secret_key
+}
+
